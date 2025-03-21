6459cf425f07c1087f3c345bc5e6d61748e93b27
chanchai <cjiceko@gmail.com>
2025-03-21 10:37:15 +0700

    add product repo

---
+++ internal/repositories/repo_product.go
@@ -0,0 +1,16 @@
+package repositories
+
+import (
+	"arczed/internal/entities/schemas"
+)
+
+func (r *constDB) AddProduct(req *schemas.ProductReq) error {
+
+	if req.ProductId == 0 {
+
+	} else {
+
+	}
+
+	return nil
+}
